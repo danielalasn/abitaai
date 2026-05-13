ee762941ba5a95689c1ba42ba8095d08ab06a6b0
alnovu <info@alnovu.com>
2026-05-13 16:50:51 -0600

    feat: use instagram config variable for whatsapp login

--- src/app/(dashboard)/settings/page.tsx
+++ src/app/(dashboard)/settings/page.tsx
@@ -321,7 +321,7 @@ export default function SettingsPage() {
           }
         },
         {
-          config_id: process.env.NEXT_PUBLIC_FB_CONFIG_WHATSAPP,
+          config_id: process.env.NEXT_PUBLIC_FB_CONFIG_INSTAGRAM,
           response_type: 'code',
           override_default_response_type: true,
           extras: {
