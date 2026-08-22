1b69ae4a2b27d1d4390ad42c75b24767f1ecb767
danielalasn <info@alnovu.com>
2026-08-21 20:10:03 -0600

    fix: ignore ffmpeg from webpack build to prevent dynamic require errors

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  serverExternalPackages: ['@ffmpeg-installer/ffmpeg', 'fluent-ffmpeg'],
   experimental: {
     serverActions: {
       bodySizeLimit: '15mb',
